84e780a95f30bee41030d3c361a56753a1f9f597
Chim Theara <chimtheara93@gmail.com>
2025-10-09 22:11:00 +0700

    user feature v1

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,10 +1,9 @@
 import type React from "react"
-import ProtectedRoute from "@/components/auth/protected-route"
 
 export default function ProfileLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <ProtectedRoute requiredRole="public">{children}</ProtectedRoute>
+  return <>{children}</>
 }
